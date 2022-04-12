5df9295465c41c15bef9ee96e25008b9d46e4f48
Corey Schadeck <corey@schadeck.com>
2022-04-11 20:51:31 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+function writeCards(names, event) {
+    let message = [];
+    for(let i= 0; i< names.length; i++) {
+    message.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+ } 
+return message;
+}
+//countdown v
+
+function countDown(positiveNumber) {
+    while(positiveNumber >= 0) {
+        console.log(positiveNumber);
+        positiveNumber--;
+    }
+}
